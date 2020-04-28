9d8276eabc1d2a56c2bec2d4fdaaf58d724715a1
Baljinder Singh Sidhu <Baljinder.S.Sidhu@aexp.com>
2020-04-29 02:31:10 +0530

    Updates

---
+++ precache-manifest.a9601cfd1082fb452dc6915141a9575a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6e3af1588773c213f0ec47496f07cb",
+    "url": "/animated-sidenav/index.html"
+  },
+  {
+    "revision": "73bdfd58a845a69295f3",
+    "url": "/animated-sidenav/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f511b8ca4043fca60a79",
+    "url": "/animated-sidenav/static/js/2.86f097b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animated-sidenav/static/js/2.86f097b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73bdfd58a845a69295f3",
+    "url": "/animated-sidenav/static/js/main.8b15fba4.chunk.js"
+  },
+  {
+    "revision": "d94033c18e5e73bea8f6",
+    "url": "/animated-sidenav/static/js/runtime-main.b2f14eec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/animated-sidenav/static/media/logo.5d5d9eef.svg"
+  }
+]);
